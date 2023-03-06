5416275cafbb515c02b8c050ae7aa1699cc4207d
ratiry <ratiry@yandex.ru>
2023-03-06 15:49:50 +0300

    made form for welcome page

--- src/Pages/Welcome/Welcome.jsx
+++ src/Pages/Welcome/Welcome.jsx
@@ -5,33 +5,57 @@ import { useForm } from "react-hook-form";
 import { ButtonWithText } from "../../Components/Buttons/Buttons";
 import { useNavigate } from "react-router-dom";
 import { URLs } from "../../App";
+import { useEffect, useState } from "react";
 let Welcome=(props)=>{
   let navigate=useNavigate();
+  let ComputerPlayers=[];
+  let computerPlayersHtml=[];
+  let preffiledValues={
+    ammountOfPlayers:'2'
+  }
   const  {
     register,
     formState:{
       errors
     },
-    handleSubmit
-  } = useForm()
+    watch,
+    handleSubmit,
+  } = useForm({
+    defaultValues:preffiledValues
+  })
+  let watchFields = watch("ammountOfPlayers"); 
   const onSubmit=(data)=>{
     console.log(data)
-    debugger;
     navigate(URLs.game,{
       state:data
     })
   }
+
+  for(let i=0;i<Number(watchFields);i++){
+    ComputerPlayers.push(i);
+  }
+  computerPlayersHtml=ComputerPlayers.map(O=>{ return <option>{O}</option>})
+  window.ComputerPlayers=ComputerPlayers
+  window.watchFields=watchFields;
   return(
     <Container>
       <H1>Domino</H1>
       <form  onSubmit={handleSubmit(onSubmit) }>
+        <div className={classes.Items}>
+          <div className={classes.Item}>
+            <P>How many players</P>
+            <select  name="ammountOfPlayers" {...register('ammountOfPlayers')}>
+              <option >2</option>
+              <option >3</option>
+              <option >4</option>
+          </select>
+        </div>
         <div className={classes.Item}>
-          <P>How many players</P>
-          <select  {...register('ammountOfPlayers')}>
-          <option >2</option>
-          <option >3</option>
-          <option >4</option>
-        </select>
+          <P>How many computer players</P>
+          <select name="ammountOfComputerPlayers" {...register("ammountOfComputerPlayers")}>
+            {computerPlayersHtml}
+          </select>
+        </div>
         </div>
         <ButtonWithText>Play</ButtonWithText>
       </form>
